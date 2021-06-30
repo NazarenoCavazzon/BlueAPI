91712f1f8fb5167ba57d942bee1756a752f19189
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2021-06-30 20:23:02 -0300

    Refactoring y agregado de cotizaciones de Dólar y Real

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 from flask import Flask, jsonify
 from datetime import datetime
 
-def getEuroValues():
+DOLAR_URL = 'https://www.paralelohoy.com.ar/p/cotizacion-dolar-hoy-argentina.html'
+EURO_URL = 'https://www.paralelohoy.com.ar/p/cotizacion-euro-hoy-argentina.html'
+REAL_URL = 'https://www.paralelohoy.com.ar/p/cotizacion-real-hoy-argentina.html'
+
+def getValues(url):
     import requests
     from bs4 import BeautifulSoup
-
-    url = 'https://www.paralelohoy.com.ar/p/cotizacion-euro-hoy-argentina.html'
 
     html_source = requests.get(url).text
     soup = BeautifulSoup(html_source, 'lxml')
@@ -24,6 +26,13 @@ def getEuroValues():
 
     return list
 
+def formatResponse(value):
+    return {
+        "fecha": datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
+        "compra" : f"{value[0]}",
+        "venta" : f"{value[1]}"
+    }
+
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
@@ -31,25 +40,41 @@ app.config['JSON_SORT_KEYS'] = False
 def ping():
     return 'pong'
 
+@app.route("/api/dolar/oficial")
+def getDolarOficial():
+    dolarValues = getValues(DOLAR_URL)
+    dolarOficial = formatResponse(dolarValues[0])
+    return jsonify(dolarOficial)
+
+@app.route("/api/dolar/blue")
+def getDolarBlue():
+    dolarValues = getValues(DOLAR_URL)
+    dolarBlue = formatResponse(dolarValues[1])
+    return jsonify(dolarBlue)
+
 @app.route("/api/euro/oficial")
 def getEuroOficial():
-    euroValues = getEuroValues()
-    euroNacion = {
-        "fecha": datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
-        "compra" : f"{euroValues[0][0]}",
-        "venta" : f"{euroValues[0][1]}"
-    }
-    return jsonify(euroNacion)
+    euroValues = getValues(EURO_URL)
+    euroOficial = formatResponse(euroValues[0])
+    return jsonify(euroOficial)
 
 @app.route("/api/euro/blue")
 def getEuroBlue():
-    euroValues = getEuroValues()
-    euroBlue = {
-        "fecha": datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
-        "compra" : f"{euroValues[1][0]}",
-        "venta" : f"{euroValues[1][1]}"
-    }
+    euroValues = getValues(EURO_URL)
+    euroBlue = formatResponse(euroValues[1])
     return jsonify(euroBlue)
+
+@app.route("/api/real/oficial")
+def getRealOficial():
+    realValues = getValues(REAL_URL)
+    realOficial = formatResponse(realValues[0])
+    return jsonify(realOficial)
+
+@app.route("/api/real/blue")
+def getRealBlue():
+    realValues = getValues(REAL_URL)
+    realBlue = formatResponse(realValues[1])
+    return jsonify(realBlue)
 
 if __name__ == '__main__':
     app.run(debug=False, port=os.getenv('PORT', 5000))
