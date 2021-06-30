3eff86f071bd0fc1f06fe1fe96da81d544c73265
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2021-06-30 20:03:31 -0300

    Agregado de fecha a la respuesta de las cotizaciones
    Además, se emprolijan las variables y se devuelve un objeto en vez de una colección

--- app.py
+++ app.py
@@ -1,57 +1,55 @@
 import os
 from flask import Flask, jsonify
+from datetime import datetime
 
-
-def getValues():
+def getEuroValues():
     import requests
     from bs4 import BeautifulSoup
 
     url = 'https://www.paralelohoy.com.ar/p/cotizacion-euro-hoy-argentina.html'
 
     html_source = requests.get(url).text
-    soup = BeautifulSoup(html_source,'lxml')
+    soup = BeautifulSoup(html_source, 'lxml')
 
     table = soup.find("table")
     span = table.tbody.text
-    euros = span.split("\n")
-    lista = []
-    for euro in euros[1:3]:
-        messi = []
-        messi = euro.split(":")[1].split("$")
-        messi.pop(0)
-        lista.append(messi)
-    return lista
+    splittedSpan = span.split("\n")
+    splittedSpan = filter(None, splittedSpan)
+    list = []
+    for x in splittedSpan:
+        value = []
+        value = x.split(":")[1].split("$")
+        value.pop(0)
+        list.append(value)
 
-def main():
-    lista = getValues()
-
-    euroNacion = [
-        {"Compra" : f"{lista[0][0]}",
-        "Venta" : f"{lista[0][1]}"}
-    ]
-
-    euroBlue = [
-        {"Compra" : f"{lista[1][0]}",
-        "Venta" : f"{lista[1][1]}"}
-        ]
-
-    return euroBlue, euroNacion
-
-euroBlue, euroNacion = main()
+    return list
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
-@app.route("/ping")
+@app.route("/api/ping")
 def ping():
     return 'pong'
 
 @app.route("/api/euro/oficial")
-def getOficial():
+def getEuroOficial():
+    euroValues = getEuroValues()
+    euroNacion = {
+        "fecha": datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
+        "compra" : f"{euroValues[0][0]}",
+        "venta" : f"{euroValues[0][1]}"
+    }
     return jsonify(euroNacion)
 
 @app.route("/api/euro/blue")
-def getBlue():
+def getEuroBlue():
+    euroValues = getEuroValues()
+    euroBlue = {
+        "fecha": datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
+        "compra" : f"{euroValues[1][0]}",
+        "venta" : f"{euroValues[1][1]}"
+    }
     return jsonify(euroBlue)
 
 if __name__ == '__main__':
-    app.run(debug=False,port=os.getenv('PORT', 5000))
+    app.run(debug=False, port=os.getenv('PORT', 5000))
