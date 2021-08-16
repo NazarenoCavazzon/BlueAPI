dd017b8ec6417622293574b8fbff62a80f778b61
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2021-08-16 01:10:22 -0300

    Update busStops.py

--- busStops.py
+++ busStops.py
@@ -1,33 +1,33 @@
 busStopsDict = {
-        {
+        "bus_stop_liniers_belgrano": {
             "lines": ["1","1B","2","3","4","5","6"],
             "name": "Parada Liniers-Belgrano",
             "address": "Liniers 10",
             "lat": -31.6584,
             "lon": -64.42795
         },
-        {
+        "bus_stop_liniers_españa": {
             "lines": ["1","1B","2","3","4","6"],
             "name": "Parada Liniers-España",
             "address": "Liniers 290",
             "lat": -31.6603,
             "lon": -64.42912
         },
-        {
+        "bus_stop_hospital1": {
             "lines": ["1","1B","2","3","6"],
             "name": "Hospital Alcrucero",
             "address": "Av. Libertador 1460",
             "lat": -31.65859,
             "lon": -64.41615
         },
-        {
+        "bus_stop_liniers490": {
             "lines": ["1","1B","2","3","4","6"],
             "name": "Parada Liniers 490",
             "address": "Liniers 490",
             "lat": -31.66238,
             "lon": -64.43034
         },
-        {
+        "bus_stop_plaza_solares": {
             "lines": ["1","1B","2","3","4","5","6"],
             "name": "Parada Plaza Solares",
             "address": "Rafael Lozada 35",
