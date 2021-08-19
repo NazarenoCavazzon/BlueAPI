4dc04b67becc8a0a5f043470b8e8533d1674eefd
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2021-08-19 00:07:19 -0300

    Update busStops.py

--- busStops.py
+++ busStops.py
@@ -2,72 +2,79 @@ busStopsDict = [
         {
             "lines": ["1","1B","2","3","4","5","6"],
             "name": "Parada Liniers-Belgrano",
-            "address": "Liniers 10",
+            "address": "Liniers 10, Alta Gracia",
             "lat": -31.6584,
             "lon": -64.42795
         },
         {
             "lines": ["1","1B","2","3","4","6"],
             "name": "Parada Liniers-España",
-            "address": "Liniers 290",
+            "address": "Liniers 290, Alta Gracia",
             "lat": -31.6603,
             "lon": -64.42912
         },
         {
             "lines": ["1","1B","2","3","6"],
             "name": "Hospital Alcrucero",
-            "address": "Av. Libertador 1460",
+            "address": "Av. Libertador 1460, Alta Gracia",
             "lat": -31.65859,
             "lon": -64.41615
         },
         {
             "lines": ["1","1B","2","3","4","6"],
-            "name": "Parada Liniers 490",
-            "address": "Liniers 490",
+            "name": "Parada Liniers - 490",
+            "address": "Liniers 490, Alta Gracia",
             "lat": -31.66238,
             "lon": -64.43034
         },
         {
             "lines": ["1","1B","2","3","4","5","6"],
             "name": "Parada Plaza Solares",
-            "address": "Rafael Lozada 35",
+            "address": "Rafael Lozada 35, Alta Gracia",
             "lat": -31.65804,
             "lon": -64.43307
         },
         {
             "lines": ["5"],
-            "name": "Prudencio Bustos 50",
-            "address": "Prudencio Bustos 50",
+            "name": "Prudencio Bustos - 50",
+            "address": "Prudencio Bustos 50, Alta Gracia",
             "lat": -31.65740,
             "lon": -64.42898
         },
         {
             "lines": ["1","2","3","4","5"],
             "name": "Parada El Alto",
-            "address": "Av. Sarmiento 450",
+            "address": "Av. Sarmiento 450, Alta Gracia",
             "lat": -31.65527,
             "lon": -64.44006
         },
         {
             "lines": ["1","3","6"],
             "name": "Parada El Obraje",
-            "address": "Av. Lucas V. Cordoba 1330",
+            "address": "Av. Lucas V. Cordoba 1330, Alta Gracia",
             "lat": -31.64427,
             "lon": -64.4339
         },
         {
             "lines": ["3"],
-            "name": "Sanavirones 490",
-            "address": "Av. Sanavirones 490",
+            "name": "Sanavirones - 490",
+            "address": "Av. Sanavirones 490, Alta Gracia",
             "lat": -31.63004,
             "lon": -64.4371
         },
         {
             "lines": ["6"],
-            "name": "Los Comechingones 30",
-            "address": "Bv. Los Comechingones 30",
+            "name": "Los Comechingones - 30",
+            "address": "Bv. Los Comechingones 30, Alta Gracia",
             "lat": -31.64067,
             "lon": -64.43303
+        },
+        {
+            "lines": ["3"],
+            "name": "Bahia Esq. Peña",
+            "address": "Bahia Blanca 1390, Alta Gracia",
+            "lat": -31.64566,
+            "lon": -64.41583
         },
 ]
 
