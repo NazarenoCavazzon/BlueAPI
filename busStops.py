492a34e2946e722dfe97507bfa5396b3195067c5
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2021-08-19 21:26:00 -0300

    Update busStops.py

--- busStops.py
+++ busStops.py
@@ -2,6 +2,7 @@ busStopsDict = [
         {
             "lines": ["1","1B","2","3","4","5","6"],
             "name": "Parada Liniers-Belgrano",
+            "subtitle": "",
             "address": "Liniers 10, Alta Gracia",
             "lat": -31.6584,
             "lon": -64.42795
@@ -9,6 +10,7 @@ busStopsDict = [
         {
             "lines": ["1","1B","2","3","4","6"],
             "name": "Parada Liniers-España",
+            "subtitle": "",
             "address": "Liniers 290, Alta Gracia",
             "lat": -31.6603,
             "lon": -64.42912
@@ -16,6 +18,7 @@ busStopsDict = [
         {
             "lines": ["1","1B","2","3","6"],
             "name": "Hospital Alcrucero",
+            "subtitle": "",
             "address": "Av. Libertador 1460, Alta Gracia",
             "lat": -31.65859,
             "lon": -64.41615
@@ -23,6 +26,7 @@ busStopsDict = [
         {
             "lines": ["1","1B","2","3","4","6"],
             "name": "Parada Liniers - 490",
+            "subtitle": "",
             "address": "Liniers 490, Alta Gracia",
             "lat": -31.66238,
             "lon": -64.43034
@@ -30,6 +34,7 @@ busStopsDict = [
         {
             "lines": ["1","1B","2","3","4","5","6"],
             "name": "Parada Plaza Solares",
+            "subtitle": "",
             "address": "Rafael Lozada 35, Alta Gracia",
             "lat": -31.65804,
             "lon": -64.43307
@@ -37,6 +42,7 @@ busStopsDict = [
         {
             "lines": ["5"],
             "name": "Prudencio Bustos - 50",
+            "subtitle": "",
             "address": "Prudencio Bustos 50, Alta Gracia",
             "lat": -31.65740,
             "lon": -64.42898
@@ -44,6 +50,7 @@ busStopsDict = [
         {
             "lines": ["1","2","3","4","5"],
             "name": "Parada El Alto",
+            "subtitle": "",
             "address": "Av. Sarmiento 450, Alta Gracia",
             "lat": -31.65527,
             "lon": -64.44006
@@ -51,6 +58,7 @@ busStopsDict = [
         {
             "lines": ["1","3","6"],
             "name": "Parada El Obraje",
+            "subtitle": "",
             "address": "Av. Lucas V. Cordoba 1330, Alta Gracia",
             "lat": -31.64427,
             "lon": -64.4339
@@ -58,6 +66,7 @@ busStopsDict = [
         {
             "lines": ["3"],
             "name": "Sanavirones - 490",
+            "subtitle": "",
             "address": "Av. Sanavirones 490, Alta Gracia",
             "lat": -31.63004,
             "lon": -64.4371
@@ -65,6 +74,7 @@ busStopsDict = [
         {
             "lines": ["6"],
             "name": "Los Comechingones - 30",
+            "subtitle": "",
             "address": "Bv. Los Comechingones 30, Alta Gracia",
             "lat": -31.64067,
             "lon": -64.43303
@@ -72,6 +82,7 @@ busStopsDict = [
         {
             "lines": ["3"],
             "name": "Bahia Esq. Peña",
+            "subtitle": "",
             "address": "Bahia Blanca 1390, Alta Gracia",
             "lat": -31.64566,
             "lon": -64.41583
@@ -81,6 +92,7 @@ busStopsDict = [
 linesDict = [
     {
         "lines" : ["1","1B","2", "3", "4", "5", "6"],
+        #green, orange, yellow, red, blue, cyan, gray
         "colors": ["orange", "orange", "green", "green", "green", "yellow", "green"]
     }
 ]
