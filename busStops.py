87472e358a97db3d076d0a6aba98bd39dd335650
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2021-08-25 21:53:48 -0300

    Update busStops.py

--- busStops.py
+++ busStops.py
@@ -2,8 +2,10 @@ busStopsDict = [{'lines': ['1', '1B', '2', '3', '4', '5', '6'], 'name': 'Rafael 
 
 linesDict = [
     {
-        "lines": ["1 - Linea Suspendida", "1B", "2", "3", "4", "5", "6"],
+        "lines": ["1 - Suspendida", "1B", "2", "3", "4", "5", "6"],
         #green, orange, yellow, red, blue, cyan, gray
-        "colors": ["gray", "orange", "green", "green", "green", "yellow", "green"]
+        "colors": ["gray", "orange", "green", "green", "green", "yellow", "green"],
+        #suspended, operative
+
     }
 ]
