6bf254a4314c794d574711b2164d01243fc5a24b
Nazareno Cavazzon <cavazzonnazareno@gmail.com>
2021-08-27 01:39:31 -0300

    Update busSchedules.py

--- busSchedules.py
+++ busSchedules.py
@@ -1,4 +1,17 @@
 schedule1 = [
     ["Terminal1","Juan","Messi"],
-    ["Terminal2","Juan","Messi"]
+    ["Terminal2","Juan","Messi"],
+    ["Terminal1","Juan","Messi"],
+    ["Terminal2","Juan","Messi"],
+    ["Terminal1","Juan","Messi"],
+    ["Terminal2","Juan","Messi"],
+    ["Terminal1","Juan","Messi"],
+    ["Terminal2","Juan","Messi"],
+    ["Terminal1","Juan","Messi"],
+    ["Terminal2","Juan","Messi"],
+    ["Terminal1","Juan","Messi"],
+    ["Terminal2","Juan","Messi"],
+    ["Terminal1","Juan","Messi"],
+    ["Terminal2","Juan","Messi"],
+    
 ]
